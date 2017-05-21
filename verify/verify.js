4bbe56f8bb6b8a11997ba4c790fa0e5fac115fb4
HFCherish <hf_cherish@163.com>
2017-05-21 22:28:55 +0800

    modify verify.js to use userId rather than username

--- verify/verify.js
+++ verify/verify.js
@@ -8,10 +8,11 @@ var chai = require('chai');
 var assert = chai.assert;
 var tv4 = require('tv4');
 var endpoint = process.env.ENDPOINT;
+// var endpoint = "http://localhost:8088";
 
 console.log(endpoint);
 
-var productId, productURI, userName, orderURI, orderId;
+var productId, productURI, userName, orderURI, orderId, userId;
 
 describe("Test", function () {
   this.timeout(60000);
@@ -197,13 +198,16 @@ describe("Test", function () {
         assert.lengthOf(result.missing, 0, "Missing/unresolved JSON schema $refs (" + result.missing && result.missing.join(', ') + ") in schema: " + JSON.stringify(schema, null, 4) + " Error");
         assert.ok(result.valid, "Got unexpected response body: " + (result.error && result.error.message) + " " + JSON.stringify(schema, null, 4) + " Error");
       }
+      userURI = response.headers['location'];
+      var splits = userURI.split("/");
+      userId = splits[splits.length - 1];
       done();
     });
   });
   //
   it("POST /users/{id}/orders -> 201", function(done) {
     var options = {
-      url: endpoint + '/users/' + userName + '/orders',
+      url: endpoint + '/users/' + userId + '/orders',
       method: 'POST',
       qs: {},
       json: {
@@ -243,7 +247,7 @@ describe("Test", function () {
   //
   it("GET /users/{id}/orders -> 200", function(done) {
     var options = {
-      url: endpoint + '/users/' + userName + '/orders',
+      url: endpoint + '/users/' + userId + '/orders',
       method: 'GET',
       qs: {},
       json: "",
